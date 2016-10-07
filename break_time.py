b64d9a0de91ecb08b744367fb4ddef3d8d8f14b0
Joshua Barnett <Joshua Barnett>
2016-10-07 15:02:09 -0600

    added timer and loop

--- break_time.py
+++ break_time.py
@@ -1,1 +1,8 @@
-webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+import webbrowser
+import time
+
+count = 0
+while (count < 5):
+  time.sleep(1)
+  webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+  count = count + 1
